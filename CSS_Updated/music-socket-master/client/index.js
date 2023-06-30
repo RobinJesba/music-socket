7184723dd9a1cd29e0afae19a42a156b1b41e3ec
Ashapyapp <87185937+Ashapyapp@users.noreply.github.com>
2023-06-30 23:23:46 +0530

    changed the looks

--- CSS_Updated/music-socket-master/client/index.js
+++ CSS_Updated/music-socket-master/client/index.js
@@ -24,7 +24,7 @@ fetch(`http://${location.hostname}:8081/ip`)
     socket.onclose = () => {
       console.log("Disconnected from the WebSocket server");
     };
-});
+  });
 
 function sendMessage(payload) {
   socket.send(JSON.stringify(payload));
@@ -46,6 +46,13 @@ function updateToggle(isMinor) {
 }
 
 function renderChords(scale, isMinor) {
+  const chordHeading = document.querySelector("#ThisChord");
+  chordHeading.style.display = "block";
+  if (isMinor) {
+    chordHeading.textContent = scale + " Minor";
+  } else {
+    chordHeading.textContent = scale + " Major";
+  }
   document.querySelector("#progression-container h2").style.display = "none";
   document.querySelector("#progression").innerHTML = "";
   document.querySelector("#chords-container h2").style.display = "block";
@@ -73,14 +80,46 @@ function renderProgression(chord) {
 
 window.generateChords = (key, isMinorScale) => {
   const chords = [];
-  const noteOrder = ["C","C#","D","Eb","E","F","F#","G","G#","A","Bb","B"];
+  const noteOrder = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+  ];
   const keyIndex = noteOrder.indexOf(key);
   const minorChordTypes = ["min", "dim", "maj", "min", "min", "maj", "maj"];
   const minorIntervals = [0, 2, 3, 5, 7, 8, 10];
   const majorChordTypes = ["maj", "min", "min", "maj", "maj", "min", "dim"];
   const majorIntervals = [0, 2, 4, 5, 7, 9, 11];
+
+  // Object to map keys with flats to their corresponding index in the noteOrder array
+  const flatKeys = {
+    Ab: 8,
+    Bb: 10,
+    Db: 1,
+    Eb: 3,
+    Gb: 6,
+  };
+
+  // Check if the key is a flat key, and use the corresponding index if it is
+  const useFlatKey = flatKeys[key] !== undefined;
+  const keyIndexOffset = useFlatKey ? flatKeys[key] : keyIndex;
+
   for (let i = 0; i < 7; i++) {
-    const rootNote = noteOrder[(keyIndex + (isMinorScale ? minorIntervals[i] : majorIntervals[i])) % 12];
+    const rootNote =
+      noteOrder[
+        (keyIndexOffset +
+          (isMinorScale ? minorIntervals[i] : majorIntervals[i])) %
+          12
+      ];
     const chordType = isMinorScale ? minorChordTypes[i] : majorChordTypes[i];
     const chord = rootNote + chordType;
     chords.push(chord);
